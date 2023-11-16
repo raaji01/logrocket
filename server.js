2df8e862dc1388e72d5f7c66b2da5bd6f1312c6a
raaji01 <raajalakshumik18077@it.ssn.edu.in>
2023-11-16 15:27:20 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const fs = require('fs');
+const app = express();
+app.use(express.json());
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+//app.use(express.static(__dirname + '/UI.html'));
+//console.log("Directory----"+__dirname);
+const htmlFilePath = 'UI.html';
+
+app.get('/', (req, res) => {
+    res.sendFile(htmlFilePath);
+});
+
+app.post("/upload_files", upload.array("files"), uploadFiles);
+
+function uploadFiles(req, res) {
+    console.log(req.body);
+    console.log(req.files);
+    req.files.forEach((f) => {
+        fs.renameSync(`${f.path}`, `public/uploads/${f.originalname}`);
+      });
+    res.json({ message: "1" });
+    //console.log("uploadedfiles" +uploadedFiles);
+}
+app.listen(5000, () => {
+    console.log(`Server started...`);
+});
